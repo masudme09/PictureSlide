dcea6df2ed387daa5c8bb3fb539c1b4efe4f9b20
Masud Rana <masudme09@gmail.com>
2020-02-12 18:39:41 +0600

    Add project files.

---
+++ PictureSlide/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSlide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Picture objects from input
+            string[] allLines = File.ReadAllLines(Console.ReadLine());
+            List<picture> pictures = new List<picture>();
+
+            for(int i=1;i<allLines.Length;i++)
+            {
+                pictures.Add(new picture(allLines[i], i-1));
+            }
+
+
+        }
+    }
+
+    class picture
+    {
+        public char pictureType { get; set; }
+        public int numberOfTags  { get; set; }
+        public List<string> tags = new List<string>();
+        public int id { get; set; }
+        public picture(string lineContainPictureDef,int id)
+        {
+            this.id = id;
+            string[] splited = lineContainPictureDef.Split(' ');
+            pictureType = Convert.ToChar(splited[0]);
+            numberOfTags = Convert.ToInt32(splited[1]);
+            for (int i=2;i<splited.Length;i++)
+            {
+                tags.Add(splited[i]);
+            }
+        }
+    }
+}
